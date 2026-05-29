aec8ac2a96158665330ccd636a649548fd18abbb
Mateusz <mateusz222244@op.pl>
2026-05-29 10:58:50 +0200

    feat: account implement

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,1 +1,34 @@
 #include "Account.h"
+
+Account::Account(Account&& from) noexcept
+    : accountNumber(std::move(from.accountNumber)),
+      owner(std::move(from.owner)),
+      balance(from.balance.load()) {}
+
+Account& Account::operator=(Account&& from) noexcept {
+    if (this != &from) {
+        accountNumber = std::move(from.accountNumber);
+        owner = std::move(from.owner);
+        balance.store(from.balance.load());
+    }
+    return *this;
+}
+
+bool Account::withdraw(double amount) {
+    if (amount <= 0) return false;
+
+    // ładujemy wartość
+    double current = balance.load();
+    // sprawdza czy wartość jest taka sama jak zapamiętana, jeśli tak wykonuje current - amount, czekaj
+    while (current >= amount && !balance.compare_exchange_weak(current, current - amount)) {}
+    return (current >= amount);
+}
+
+bool Account::deposit(double amount) {
+    if (amount <= 0) return false;
+
+    // std::atomic posiada przeładowany operator +=
+    // który wykonuje operacje atomowo
+    balance += amount;
+    return true;
+}
