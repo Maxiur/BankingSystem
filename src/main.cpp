c2b38c3f3f0441dfd6cac5b8f38eab509da03749
Mateusz <mateusz222244@op.pl>
2026-05-30 15:40:29 +0200

    feat: UI add

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,9 @@
-#include "Bank.hpp"
-#include <iostream>
-#include <limits>
-#include <string>
+#include "mainwindow.h"
+#include <QApplication>
 
-void clearInputStream() {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-void showMenu() {
-    std::cout << "\n=== SYSTEM BANKOWY ===\n"
-              << "1. Utwórz konto\n"
-              << "2. Wpłata\n"
-              << "3. Wypłata\n"
-              << "4. Informacje o koncie\n"
-              << "5. Wyjście\n"
-              << "Wybierz: ";
-}
-
-int main() {
-    Bank bank;
-
-    // hajs podzielony / 100
-    bank.createAccount("0049", "Mateusz", 1025);
-    bank.printAccountInfo("0049");
-
-    bank.createAccount("0048", "Janek", 1005);
-    bank.printAccountInfo("0048");
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+    return app.exec();
 }
